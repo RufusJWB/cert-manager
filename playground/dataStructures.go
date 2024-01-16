10bafacda03fb5625bcb5032b6731478fb075840
Rufus J.W. Buschart <rufus.buschart@siemens.com>
2024-01-16 22:29:02 +0100

    -

--- playground/dataStructures.go
+++ playground/dataStructures.go
@@ -6,54 +6,50 @@ import (
 	"time"
 )
 
+const PVNO = 2
+
 type Name pkix.RDNSequence
 
-func (name Name) GeneralName() (generalName GeneralName, err error) {
+func (name Name) GeneralName(tag int) (generalName asn1.RawValue) {
 
-	var outerErr error
-
-	var temp = GeneralName{
-		Class:      asn1.ClassContextSpecific,
-		Tag:        4,
-		IsCompound: true,
-		Bytes: func() []byte {
-			b, err := asn1.Marshal(name)
-			outerErr = err
-			return b
-		}(),
-	}
-
-	return temp, outerErr
-}
-
-type IA5String string
-
-func (ia5String IA5String) GeneralName(tag int) (generalName GeneralName, err error) {
-
-	var outerErr error
-
-	var temp = GeneralName{
+	var temp = asn1.RawValue{
 		Class:      asn1.ClassContextSpecific,
 		Tag:        tag,
 		IsCompound: true,
 		Bytes: func() []byte {
-			b, err := asn1.Marshal(ia5String)
-			outerErr = err
+			b, _ := asn1.Marshal(name)
 			return b
 		}(),
 	}
 
-	return temp, outerErr
+	return temp
+}
+
+type IA5String string
+
+func (ia5String IA5String) GeneralName(tag int) (generalName asn1.RawValue) {
+
+	var temp = asn1.RawValue{
+		Class:      asn1.ClassContextSpecific,
+		Tag:        tag,
+		IsCompound: true,
+		Bytes: func() []byte {
+			b, _ := asn1.Marshal(ia5String)
+			return b
+		}(),
+	}
+
+	return temp
 }
 
 type GeneralName asn1.RawValue
 
 type PKIHeader struct {
 	PVNO          int
-	Sender        GeneralName
-	Recipient     GeneralName
-	MessageTime   time.Time                `asn1:"generalized,optional,tag:0,omitempty"`
-	ProtectionAlg pkix.AlgorithmIdentifier `asn1:"optional,tag:1,omitempty"`
+	Sender        asn1.RawValue
+	Recipient     asn1.RawValue
+	MessageTime   time.Time                `asn1:"generalized,explicit,optional,tag:0,omitempty"`
+	ProtectionAlg pkix.AlgorithmIdentifier `asn1:"explicit,optional,tag:1,omitempty"`
 	//SendKID       any                          `asn1:"optional,tag:2,omitempty"`
 	//RecipKID      any                          `asn1:"optional,tag:3,omitempty"`
 	//TransactionID []byte                       `asn1:"optional,tag:4,omitempty"`
@@ -63,12 +59,11 @@ type PKIHeader struct {
 	//GeneralInfo   []pkix.AttributeTypeAndValue `asn1:"optional,tag:8,omitempty"`
 }
 
-type PKIBody asn1.RawValue
-
+type PKIBody any
 
 type PKIProtection asn1.BitString
 
-type CMPCertificate struct{}
+type CMPCertificate any
 
 type PKIMessage struct {
 	Header     PKIHeader
--- playground/test.go
+++ playground/test.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"crypto/x509/pkix"
 	"encoding/asn1"
 	"encoding/base64"
 	"fmt"
+	"time"
 )
 
 type Type1 struct {
@@ -22,104 +24,105 @@ type MyStructure struct {
 }
 
 func main() {
-	// Create a structure with a Type1 choice
-	myStruct1 := MyStructure{
-		ID: 1,
-		Choice: asn1.RawValue{
-			Class:      asn1.ClassContextSpecific,
-			Tag:        0,
-			IsCompound: true,
-			Bytes: func() []byte {
-				b, _ := asn1.Marshal(Type1{ID: 42, Value: []byte("Hello, world!")})
-				return b
-			}(),
+
+	country := "DE"
+	organization := "Siemens"
+	commonName := "www.siemens.com"
+
+	/*
+		senderAsName := Name{[]pkix.AttributeTypeAndValue{
+			{Type: asn1.ObjectIdentifier{2, 5, 4, 6}, Value: country},},
+			[]pkix.AttributeTypeAndValue{
+			{Type: asn1.ObjectIdentifier{2, 5, 4, 10}, Value: organization},},
+			[]pkix.AttributeTypeAndValue{
+			{Type: asn1.ObjectIdentifier{2, 5, 4, 3}, Value: commonName}}}
+
+		senderAsGeneralName := senderAsName.GeneralName(4);
+	*/
+
+	testStruct := PKIMessage{
+		Header: PKIHeader{
+			PVNO: PVNO,
+			Sender: (Name{[]pkix.AttributeTypeAndValue{
+				{Type: asn1.ObjectIdentifier{2, 5, 4, 6}, Value: country}},
+				[]pkix.AttributeTypeAndValue{
+					{Type: asn1.ObjectIdentifier{2, 5, 4, 10}, Value: organization}},
+				[]pkix.AttributeTypeAndValue{
+					{Type: asn1.ObjectIdentifier{2, 5, 4, 3}, Value: commonName}}}).GeneralName(4),
+			Recipient: (Name{[]pkix.AttributeTypeAndValue{
+				{Type: asn1.ObjectIdentifier{2, 5, 4, 6}, Value: country}},
+				[]pkix.AttributeTypeAndValue{
+					{Type: asn1.ObjectIdentifier{2, 5, 4, 10}, Value: organization}},
+				[]pkix.AttributeTypeAndValue{
+					{Type: asn1.ObjectIdentifier{2, 5, 4, 3}, Value: commonName}}}).GeneralName(4),
+			MessageTime:   time.Now(),
+			ProtectionAlg: pkix.AlgorithmIdentifier{
+				Algorithm:  asn1.ObjectIdentifier{ 1,2,840,113533,7,66,13 }, // --> https://github.com/zjj/golibkit/blob/main/certutil/helper.go
+				Parameters: asn1.NullRawValue,
+			},
 		},
+		Body:       []int{},
+		Protection: PKIProtection{},
+		ExtraCerts: []CMPCertificate{},
 	}
 
-	// Create a structure with a Type2 choice
-	myStruct2 := MyStructure{
-		ID: 2,
-		Choice: asn1.RawValue{
-			Class:      asn1.ClassContextSpecific,
-			Tag:        1,
-			IsCompound: true,
-			Bytes: func() []byte {
-				b, _ := asn1.Marshal(Type2{ID: 43, Value: "Hello, ASN.1!"})
-				return b
-			}(),
-		},
-	}
-
-	// Marshal to ASN.1 DER
-	bytes1, err1 := asn1.Marshal(myStruct1)
+	bytes1, err1 := asn1.Marshal(testStruct)
 	if err1 != nil {
 		fmt.Println("Error marshaling structure 1:", err1)
 		return
 	}
 
-	bytes2, err2 := asn1.Marshal(myStruct2)
-	if err2 != nil {
-		fmt.Println("Error marshaling structure 2:", err2)
-		return
-	}
-
-	// Encode to base64
 	base64Str1 := base64.StdEncoding.EncodeToString(bytes1)
-	base64Str2 := base64.StdEncoding.EncodeToString(bytes2)
-
 	fmt.Println(base64Str1)
-	fmt.Println(base64Str2)
+	//fmt.Println(base64Str2)
 
 	// Decode from base64
-	decodedBytes1, _ := base64.StdEncoding.DecodeString(base64Str1)
-	decodedBytes2, _ := base64.StdEncoding.DecodeString(base64Str2)
+	//decodedBytes1, _ := base64.StdEncoding.DecodeString(base64Str1)
+	//decodedBytes2, _ := base64.StdEncoding.DecodeString(base64Str2)
 
-	// Unmarshal from ASN.1 DER
-	var unmarshaledStruct1, unmarshaledStruct2 MyStructure
-	_, err3 := asn1.Unmarshal(decodedBytes1, &unmarshaledStruct1)
-	_, err4 := asn1.Unmarshal(decodedBytes2, &unmarshaledStruct2)
-	if err3 != nil || err4 != nil {
-		fmt.Println("Error unmarshaling structures:", err3, err4)
-		return
-	}
+	/*
+		// Unmarshal from ASN.1 DER
+		var unmarshaledStruct1, unmarshaledStruct2 MyStructure
+		_, err3 := asn1.Unmarshal(decodedBytes1, &unmarshaledStruct1)
 
-	// Unmarshal the choice based on the tag
-	switch unmarshaledStruct1.Choice.Tag {
-	case 0:
-		var choice1 Type1
-		_, err := asn1.Unmarshal(unmarshaledStruct1.Choice.Bytes, &choice1)
-		if err != nil {
-			fmt.Println("Error unmarshaling choice1:", err)
-			return
-		}
-		fmt.Println(choice1)
-	case 1:
-		var choice2 Type2
-		_, err := asn1.Unmarshal(unmarshaledStruct1.Choice.Bytes, &choice2)
-		if err != nil {
-			fmt.Println("Error unmarshaling choice2:", err)
-			return
-		}
-		fmt.Println(choice2)
-	}
 
-	// Unmarshal the choice based on the tag
-	switch unmarshaledStruct2.Choice.Tag {
-	case 0:
-		var choice1 Type1
-		_, err := asn1.Unmarshal(unmarshaledStruct2.Choice.Bytes, &choice1)
-		if err != nil {
-			fmt.Println("Error unmarshaling choice1:", err)
-			return
+		// Unmarshal the choice based on the tag
+		switch unmarshaledStruct1.Choice.Tag {
+		case 0:
+			var choice1 Type1
+			_, err := asn1.Unmarshal(unmarshaledStruct1.Choice.Bytes, &choice1)
+			if err != nil {
+				fmt.Println("Error unmarshaling choice1:", err)
+				return
+			}
+			fmt.Println(choice1)
+		case 1:
+			var choice2 Type2
+			_, err := asn1.Unmarshal(unmarshaledStruct1.Choice.Bytes, &choice2)
+			if err != nil {
+				fmt.Println("Error unmarshaling choice2:", err)
+				return
+			}
+			fmt.Println(choice2)
 		}
-		fmt.Println(choice1)
-	case 1:
-		var choice2 Type2
-		_, err := asn1.Unmarshal(unmarshaledStruct2.Choice.Bytes, &choice2)
-		if err != nil {
-			fmt.Println("Error unmarshaling choice2:", err)
-			return
-		}
-		fmt.Println(choice2)
-	}
+
+		// Unmarshal the choice based on the tag
+		switch unmarshaledStruct2.Choice.Tag {
+		case 0:
+			var choice1 Type1
+			_, err := asn1.Unmarshal(unmarshaledStruct2.Choice.Bytes, &choice1)
+			if err != nil {
+				fmt.Println("Error unmarshaling choice1:", err)
+				return
+			}
+			fmt.Println(choice1)
+		case 1:
+			var choice2 Type2
+			_, err := asn1.Unmarshal(unmarshaledStruct2.Choice.Bytes, &choice2)
+			if err != nil {
+				fmt.Println("Error unmarshaling choice2:", err)
+				return
+			}
+			fmt.Println(choice2)
+	*/
 }
