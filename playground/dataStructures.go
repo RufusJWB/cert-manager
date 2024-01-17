6a91b08976794ca1cfbb73a31ded3561ec7e0445
Rufus J.W. Buschart <rufus.buschart@siemens.com>
2024-01-17 17:18:18 +0100

    -

--- playground/dataStructures.go
+++ playground/dataStructures.go
@@ -6,15 +6,60 @@ import (
 	"time"
 )
 
-const PVNO = 2
+var (
+	oidCountry            = asn1.ObjectIdentifier{2, 5, 4, 6}
+	oidOrganization       = asn1.ObjectIdentifier{2, 5, 4, 10}
+	oidOrganizationalUnit = asn1.ObjectIdentifier{2, 5, 4, 11}
+	oidCommonName         = asn1.ObjectIdentifier{2, 5, 4, 3}
+	oidSerialNumber       = asn1.ObjectIdentifier{2, 5, 4, 5}
+	oidLocality           = asn1.ObjectIdentifier{2, 5, 4, 7}
+	oidProvince           = asn1.ObjectIdentifier{2, 5, 4, 8}
+	oidStreetAddress      = asn1.ObjectIdentifier{2, 5, 4, 9}
+	oidPostalCode         = asn1.ObjectIdentifier{2, 5, 4, 17}
+)
+
+var (
+	oidSignatureSHA256WithRSA   = asn1.ObjectIdentifier{1, 2, 840, 113549, 1, 1, 11}
+	oidSignatureSHA384WithRSA   = asn1.ObjectIdentifier{1, 2, 840, 113549, 1, 1, 12}
+	oidSignatureSHA512WithRSA   = asn1.ObjectIdentifier{1, 2, 840, 113549, 1, 1, 13}
+	oidSignatureRSAPSS          = asn1.ObjectIdentifier{1, 2, 840, 113549, 1, 1, 10}
+	oidSignatureDSAWithSHA256   = asn1.ObjectIdentifier{2, 16, 840, 1, 101, 3, 4, 3, 2}
+	oidSignatureECDSAWithSHA256 = asn1.ObjectIdentifier{1, 2, 840, 10045, 4, 3, 2}
+	oidSignatureECDSAWithSHA384 = asn1.ObjectIdentifier{1, 2, 840, 10045, 4, 3, 3}
+	oidSignatureECDSAWithSHA512 = asn1.ObjectIdentifier{1, 2, 840, 10045, 4, 3, 4}
+
+	oidHMACWithSHA256 = asn1.ObjectIdentifier{1, 2, 840, 113549, 2, 9}
+	oidHMACWithSHA384 = asn1.ObjectIdentifier{1, 2, 840, 113549, 2, 10}
+	oidHMACWithSHA512 = asn1.ObjectIdentifier{1, 2, 840, 113549, 2, 11}
+
+	oidSHA256 = asn1.ObjectIdentifier{2, 16, 840, 1, 101, 3, 4, 2, 1}
+	oidSHA384 = asn1.ObjectIdentifier{2, 16, 840, 1, 101, 3, 4, 2, 2}
+	oidSHA512 = asn1.ObjectIdentifier{2, 16, 840, 1, 101, 3, 4, 2, 3}
+
+	oidPBM = asn1.ObjectIdentifier{1, 2, 840, 113533, 7, 66, 13}
+)
+
+/*
+   PKIFreeText ::= SEQUENCE SIZE (1..MAX) OF UTF8String
+       -- text encoded as UTF-8 String [RFC3629] (note: each
+       -- UTF8String MAY include an [RFC3066] language tag
+       -- to indicate the language of the contained text
+       -- see [RFC2482] for details)
+*/
+
+type PKIFreeText []string
 
 type Name pkix.RDNSequence
 
-func (name Name) GeneralName(tag int) (generalName asn1.RawValue) {
+func (name Name) String() (result string) {
+	return pkix.RDNSequence(name).String()
+}
+
+func (name Name) GeneralName(contextSpecificTag int) (generalName asn1.RawValue) {
 
 	var temp = asn1.RawValue{
 		Class:      asn1.ClassContextSpecific,
-		Tag:        tag,
+		Tag:        contextSpecificTag,
 		IsCompound: true,
 		Bytes: func() []byte {
 			b, _ := asn1.Marshal(name)
@@ -27,11 +72,11 @@ func (name Name) GeneralName(tag int) (generalName asn1.RawValue) {
 
 type IA5String string
 
-func (ia5String IA5String) GeneralName(tag int) (generalName asn1.RawValue) {
+func (ia5String IA5String) GeneralName(contextSpecificTag int) (generalName asn1.RawValue) {
 
 	var temp = asn1.RawValue{
 		Class:      asn1.ClassContextSpecific,
-		Tag:        tag,
+		Tag:        contextSpecificTag,
 		IsCompound: true,
 		Bytes: func() []byte {
 			b, _ := asn1.Marshal(ia5String)
@@ -44,21 +89,127 @@ func (ia5String IA5String) GeneralName(tag int) (generalName asn1.RawValue) {
 
 type GeneralName asn1.RawValue
 
+/*
+GeneralName ::= CHOICE {
+	otherName                       [0]     AnotherName,
+	rfc822Name                      [1]     IA5String,
+	dNSName                         [2]     IA5String,
+	x400Address                     [3]     ORAddress,
+	directoryName                   [4]     Name,
+	ediPartyName                    [5]     EDIPartyName,
+	uniformResourceIdentifier       [6]     IA5String,
+	iPAddress                       [7]     OCTET STRING,
+	registeredID                    [8]     OBJECT IDENTIFIER }
+*/
+
+const (
+	otherName = iota
+	rfc822Name
+	dNSName
+	x400Address
+	directoryName
+	ediPartyName
+	uniformResourceIdentifier
+	iPAddress
+	registeredID
+)
+
 type KeyIdentifier []byte
+
+/*
+   PBMParameter ::= SEQUENCE {
+       salt                OCTET STRING,
+       -- note:  implementations MAY wish to limit acceptable sizes
+       -- of this string to values appropriate for their environment
+       -- in order to reduce the risk of denial-of-service attacks
+       owf                 AlgorithmIdentifier,
+       -- AlgId for a One-Way Function (SHA-1 recommended)
+       iterationCount      INTEGER,
+       -- number of times the OWF is applied
+       -- note:  implementations MAY wish to limit acceptable sizes
+       -- of this integer to values appropriate for their environment
+       -- in order to reduce the risk of denial-of-service attacks
+       mac                 AlgorithmIdentifier
+       -- the MAC AlgId (e.g., DES-MAC, Triple-DES-MAC [PKCS11],
+   }   -- or HMAC [RFC2104, RFC2202])
+*/
+
+type PBMParameter struct {
+	Salt           []byte
+	OWF            AlgorithmIdentifier
+	IterationCount int
+	MAC            AlgorithmIdentifier
+}
+
+/*
+AlgorithmIdentifier  ::=  SEQUENCE  {
+	algorithm               OBJECT IDENTIFIER,
+	parameters              ANY DEFINED BY algorithm OPTIONAL  }
+							   -- contains a value of the type
+							   -- registered for use with the
+							   -- algorithm object identifier value
+*/
+
+type AlgorithmIdentifier struct {
+	Algorithm  asn1.ObjectIdentifier
+	Parameters any `asn1:"optional,omitempty"`
+}
+
+const (
+	CMP1999 = iota
+	CMP2000
+	CMP2021
+)
+
+/*
+PKIHeader ::= SEQUENCE {
+         pvno                INTEGER     { cmp1999(1), cmp2000(2) },
+         sender              GeneralName,
+         -- identifies the sender
+         recipient           GeneralName,
+         -- identifies the intended recipient
+         messageTime     [0] GeneralizedTime         OPTIONAL,
+         -- time of production of this message (used when sender
+         -- believes that the transport will be "suitable"; i.e.,
+         -- that the time will still be meaningful upon receipt)
+         protectionAlg   [1] AlgorithmIdentifier     OPTIONAL,
+         -- algorithm used for calculation of protection bits
+         senderKID       [2] KeyIdentifier           OPTIONAL,
+         recipKID        [3] KeyIdentifier           OPTIONAL,
+         -- to identify specific keys used for protection
+         transactionID   [4] OCTET STRING            OPTIONAL,
+         -- identifies the transaction; i.e., this will be the same in
+         -- corresponding request, response, certConf, and PKIConf
+         -- messages
+         senderNonce     [5] OCTET STRING            OPTIONAL,
+         recipNonce      [6] OCTET STRING            OPTIONAL,
+         -- nonces used to provide replay protection, senderNonce
+         -- is inserted by the creator of this message; recipNonce
+         -- is a nonce previously inserted in a related message by
+         -- the intended recipient of this message
+         freeText        [7] PKIFreeText             OPTIONAL,
+         -- this may be used to indicate context-specific instructions
+         -- (this field is intended for human consumption)
+         generalInfo     [8] SEQUENCE SIZE (1..MAX) OF
+                                InfoTypeAndValue     OPTIONAL
+         -- this may be used to convey context-specific information
+         -- (this field not primarily intended for human consumption)
+     }
+*/
 
 type PKIHeader struct {
 	PVNO          int
 	Sender        asn1.RawValue
 	Recipient     asn1.RawValue
-	MessageTime   time.Time                `asn1:"generalized,explicit,optional,tag:0,omitempty"`
-	ProtectionAlg pkix.AlgorithmIdentifier `asn1:"explicit,optional,tag:1,omitempty"`
-	SendKID       KeyIdentifier            `asn1:"optional,tag:2,omitempty"`
-	RecipKID      KeyIdentifier            `asn1:"optional,tag:3,omitempty"`
-	//TransactionID []byte                       `asn1:"optional,tag:4,omitempty"`
-	//SenderNonce   []byte                       `asn1:"optional,tag:5,omitempty"`
-	//RecipNonce    []byte                       `asn1:"optional,tag:6,omitempty"`
-	//FreeText      PKIFreeText                  `asn1:"optional,tag:7,omitempty"`
-	//GeneralInfo   []pkix.AttributeTypeAndValue `asn1:"optional,tag:8,omitempty"`
+	MessageTime   time.Time           `asn1:"generalized,explicit,optional,tag:0,omitempty"`
+	ProtectionAlg AlgorithmIdentifier `asn1:"explicit,optional,tag:1,omitempty"`
+	SenderKID     KeyIdentifier       `asn1:"explicit,optional,tag:2,omitempty"`
+	RecipientKID  KeyIdentifier       `asn1:"explicit,optional,tag:3,omitempty"`
+	TransactionID []byte              `asn1:"explicit,optional,tag:4,omitempty"`
+	SenderNonce   []byte              `asn1:"explicit,optional,tag:5,omitempty"`
+	RecipNonce    []byte              `asn1:"explicit,optional,tag:6,omitempty"`
+	// FreeText      PKIFreeText         `asn1:"explicit,optional,tag:7,omitempty"` // Not working
+	// GeneralInfo   []pkix.AttributeTypeAndValue `asn1:"explicit,optional,tag:8,omitempty"` // Not working
 }
 
 type PKIBody any
@@ -67,9 +218,19 @@ type PKIProtection asn1.BitString
 
 type CMPCertificate any
 
+/*
+      PKIMessage ::= SEQUENCE {
+         header           PKIHeader,
+         body             PKIBody,
+         protection   [0] PKIProtection OPTIONAL,
+         extraCerts   [1] SEQUENCE SIZE (1..MAX) OF CMPCertificate
+                          OPTIONAL
+	  }
+*/
+
 type PKIMessage struct {
 	Header     PKIHeader
 	Body       PKIBody
-	Protection PKIProtection    `asn1:"optional,tag:0,omitempty"`
-	ExtraCerts []CMPCertificate `asn1:"optional,tag:1,omitempty"`
+	Protection PKIProtection    `asn1:"optional,tag:0,omitempty, explicit"`
+	ExtraCerts []CMPCertificate `asn1:"optional,tag:1,omitempty, explicit"`
 }
--- playground/test.go
+++ playground/test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto/rand"
 	"crypto/x509/pkix"
 	"encoding/asn1"
 	"encoding/base64"
@@ -8,67 +9,85 @@ import (
 	"time"
 )
 
-type Type1 struct {
-	ID    int
-	Value []byte
-}
+func RandomByte(n int) ([]byte, error) {
+	b := make([]byte, n)
+	_, err := rand.Read(b)
+	// Note that err == nil only if we read len(b) bytes.
+	if err != nil {
+		return nil, err
+	}
 
-type Type2 struct {
-	ID    int
-	Value string
-}
-
-type MyStructure struct {
-	ID     int
-	Choice asn1.RawValue
+	return b, nil
 }
 
 func main() {
 
-	country := "DE"
-	organization := "Siemens"
-	commonName := "www.siemens.com"
+	senderOrganization := "Siemens"
+	senderCommonName := "CloudCA-Integration-Test-User"
 
-	/*
-		senderAsName := Name{[]pkix.AttributeTypeAndValue{
-			{Type: asn1.ObjectIdentifier{2, 5, 4, 6}, Value: country},},
-			[]pkix.AttributeTypeAndValue{
-			{Type: asn1.ObjectIdentifier{2, 5, 4, 10}, Value: organization},},
-			[]pkix.AttributeTypeAndValue{
-			{Type: asn1.ObjectIdentifier{2, 5, 4, 3}, Value: commonName}}}
+	recipientCommonName := "CloudPKI-Integration-Test"
 
-		senderAsGeneralName := senderAsName.GeneralName(4);
-	*/
+	randomSalt, err := RandomByte(16)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
-	testStruct := PKIMessage{
+	randomTransactionID, err := RandomByte(16)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	randomSenderNonce, err := RandomByte(16)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	senderDN := Name{
+		[]pkix.AttributeTypeAndValue{
+			{Type: oidOrganization, Value: senderOrganization}},
+		[]pkix.AttributeTypeAndValue{
+			{Type: oidCommonName, Value: senderCommonName}}}
+
+	recipientDN := Name{
+		[]pkix.AttributeTypeAndValue{
+			{Type: oidCommonName, Value: recipientCommonName}}}
+
+	requestMessage := PKIMessage{
 		Header: PKIHeader{
-			PVNO: PVNO,
-			Sender: (Name{[]pkix.AttributeTypeAndValue{
-				{Type: asn1.ObjectIdentifier{2, 5, 4, 6}, Value: country}},
-				[]pkix.AttributeTypeAndValue{
-					{Type: asn1.ObjectIdentifier{2, 5, 4, 10}, Value: organization}},
-				[]pkix.AttributeTypeAndValue{
-					{Type: asn1.ObjectIdentifier{2, 5, 4, 3}, Value: commonName}}}).GeneralName(4),
-			Recipient: (Name{[]pkix.AttributeTypeAndValue{
-				{Type: asn1.ObjectIdentifier{2, 5, 4, 6}, Value: country}},
-				[]pkix.AttributeTypeAndValue{
-					{Type: asn1.ObjectIdentifier{2, 5, 4, 10}, Value: organization}},
-				[]pkix.AttributeTypeAndValue{
-					{Type: asn1.ObjectIdentifier{2, 5, 4, 3}, Value: commonName}}}).GeneralName(4),
+			PVNO:        CMP2000,
+			Sender:      (senderDN).GeneralName(directoryName),
+			Recipient:   (recipientDN).GeneralName(directoryName),
 			MessageTime: time.Now(),
-			ProtectionAlg: pkix.AlgorithmIdentifier{
-				Algorithm:  asn1.ObjectIdentifier{1, 2, 840, 113533, 7, 66, 13}, // --> https://github.com/zjj/golibkit/blob/main/certutil/helper.go
-				Parameters: asn1.NullRawValue,
+			ProtectionAlg: AlgorithmIdentifier{
+				Algorithm: oidPBM,
+				Parameters: PBMParameter{
+					Salt: randomSalt,
+					OWF: AlgorithmIdentifier{
+						Algorithm:  oidSHA512,
+						Parameters: []byte{},
+					},
+					IterationCount: 1024, // Increase significantly for production!!!
+					MAC: AlgorithmIdentifier{
+						Algorithm:  oidHMACWithSHA512,
+						Parameters: []byte{},
+					},
+				},
 			},
-			SendKID:  []byte{},
-			RecipKID: []byte{},
+			SenderKID:    KeyIdentifier(senderDN.String()),
+			RecipientKID: KeyIdentifier(recipientDN.String()),
+			TransactionID: randomTransactionID,
+			SenderNonce: randomSenderNonce,
+			RecipNonce: []byte{},
 		},
 		Body:       []int{},
 		Protection: PKIProtection{},
 		ExtraCerts: []CMPCertificate{},
 	}
 
-	bytes1, err1 := asn1.Marshal(testStruct)
+	bytes1, err1 := asn1.Marshal(requestMessage)
 	if err1 != nil {
 		fmt.Println("Error marshaling structure 1:", err1)
 		return
@@ -76,55 +95,5 @@ func main() {
 
 	base64Str1 := base64.StdEncoding.EncodeToString(bytes1)
 	fmt.Println(base64Str1)
-	//fmt.Println(base64Str2)
 
-	// Decode from base64
-	//decodedBytes1, _ := base64.StdEncoding.DecodeString(base64Str1)
-	//decodedBytes2, _ := base64.StdEncoding.DecodeString(base64Str2)
-
-	/*
-		// Unmarshal from ASN.1 DER
-		var unmarshaledStruct1, unmarshaledStruct2 MyStructure
-		_, err3 := asn1.Unmarshal(decodedBytes1, &unmarshaledStruct1)
-
-
-		// Unmarshal the choice based on the tag
-		switch unmarshaledStruct1.Choice.Tag {
-		case 0:
-			var choice1 Type1
-			_, err := asn1.Unmarshal(unmarshaledStruct1.Choice.Bytes, &choice1)
-			if err != nil {
-				fmt.Println("Error unmarshaling choice1:", err)
-				return
-			}
-			fmt.Println(choice1)
-		case 1:
-			var choice2 Type2
-			_, err := asn1.Unmarshal(unmarshaledStruct1.Choice.Bytes, &choice2)
-			if err != nil {
-				fmt.Println("Error unmarshaling choice2:", err)
-				return
-			}
-			fmt.Println(choice2)
-		}
-
-		// Unmarshal the choice based on the tag
-		switch unmarshaledStruct2.Choice.Tag {
-		case 0:
-			var choice1 Type1
-			_, err := asn1.Unmarshal(unmarshaledStruct2.Choice.Bytes, &choice1)
-			if err != nil {
-				fmt.Println("Error unmarshaling choice1:", err)
-				return
-			}
-			fmt.Println(choice1)
-		case 1:
-			var choice2 Type2
-			_, err := asn1.Unmarshal(unmarshaledStruct2.Choice.Bytes, &choice2)
-			if err != nil {
-				fmt.Println("Error unmarshaling choice2:", err)
-				return
-			}
-			fmt.Println(choice2)
-	*/
 }
